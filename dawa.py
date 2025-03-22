04fa5ef0f4b3d303618b04fbb39c498cacbe749d
Dawa <rayamp711@gmail.com>
2025-03-22 12:54:16 +0100

    Add files via upload

---
+++ dawa.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+def main():
+    url = "https://discord.gg/dawafr"
+    webbrowser.open(url, new=2)  # Ouvre l'URL dans un nouvel onglet du navigateur par défaut
+
+if __name__ == "__main__":
+    main()
