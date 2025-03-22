80f9d2645ab6eb8e2e1f0626586c846a978033d3
Dawa <rayamp711@gmail.com>
2025-03-22 13:54:29 +0100

    dawa.py

--- dawa.py
+++ dawa.py
@@ -2,7 +2,7 @@ import webbrowser
 
 def main():
     url = "https://discord.gg/dawafr"
-    webbrowser.open(url, new=2)  # Ouvre l'URL dans un nouvel onglet du navigateur par défaut
+    webbrowser.open(url, new=2)
 
 if __name__ == "__main__":
     main()
